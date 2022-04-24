40109686e1c39425753bc98d85462bfc8707a8b8
Bart Strzyga <87241975+bartstr@users.noreply.github.com>
2022-04-24 12:35:32 +0200

    changed all defalut texts from main site and blog site

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="blog">
     <div class="py-8 md:py-16 text-center">
-      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Blog</h1>
+      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Short Python articles</h1>
       <h2 class="text-base md:text-lg lg:text-xl xl:text-2xl">
-        Slow-carb messenger bag mlkshk fingerstache four dollar toast.
+          Less than 3 min of reading, essential info only.
       </h2>
     </div>
 
